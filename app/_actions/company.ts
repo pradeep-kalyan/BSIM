4c6a8f2c0a4750ff3cd4652a8ea939e704b6ba8e
pradeepkalyan <pradeepkalyan1275@gmail.com>
2025-07-28 09:01:34 +0530

    ic

--- app/_actions/company.ts
+++ app/_actions/company.ts
@@ -40,6 +40,7 @@ export async function getCompaniesBySimulation(simulationId: string) {
         _count: {
           select: {
             products: true,
+            decisions: true,
           },
         },
       },
